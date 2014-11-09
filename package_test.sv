271383941ce2ecf82da3b5086be927d121143bb4
emjsmonson/em <emjsmonson@gmail.com/em>
2014-11-08 16:57:14 -0800

    stuff

--- package_test.sv
+++ package_test.sv
@@ -7,25 +7,8 @@ package package_test;
       end \
   end while(0)
   
-  class header_c;
-	
-  endclass
-  virtual class	base_packet;
-	 rand header_c header;
-  	rand data_c	data;				
-	 static int	count;	//	Number	of	instance	created
-  	int	id;	//	Unique	transaction	id
-  	function new();
-	 	id	=	count++;	//	Give	each	object	a	unique	ID
-	 	header	=	new();
-		data	=	new();
-	 endfunction	//	new
-						
-	 pure virtual	function	base_packet	copy();
-	 pure virtual	function	void display();
-	 pure virtual	function	void calc_header_checksum();
-  endclass
-
+ 
+    //Call Back Class
   virtual class Driver_cbs;
      virtual task pre_tx(ref base_packet pkt);
 	//Callback does nothing
